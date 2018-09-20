d7344fa54c5e2cb9b051d9ed31fd2f314decee46
Binh Tran <binh12121989@gmail.com>
2018-09-20 11:22:18 -0500

    Lab8 problem 1 fix

--- src/lab8/day2/prob1/Main.java
+++ src/lab8/day2/prob1/Main.java
@@ -2,9 +2,12 @@ package lab8.day2.prob1;
 
 public class Main {
 	public static void main(String... args) {
-		MyClass mc = new MyClass();
-		MyClass mc2 = new MyClass();
-		System.out.println(mc.isEquals(mc2));
+		MyClass mc = new MyClass(1, 2);
+		MyClass mc2 = new MyClass(3, 4);
+		MyClass mc3 = new MyClass(3, 4);
 		System.out.println(mc.isEquals(mc));
+		System.out.println(mc.isEquals(mc2));
+		System.out.println(mc2.isEquals(mc3));
+		System.out.println(mc3.isEquals(mc2));
 	}
 }
--- src/lab8/day2/prob1/MyClass.java
+++ src/lab8/day2/prob1/MyClass.java
@@ -1,10 +1,48 @@
 package lab8.day2.prob1;
 
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MyClass {
+	int x;
+	int y;
+
+	public MyClass(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		MyClass other = (MyClass) obj;
+		if (x != other.x) {
+			return false;
+		}
+		if (y != other.y) {
+			return false;
+		}
+		return true;
+	}
+
 	public boolean isEquals(MyClass cl) {
-		Function<MyClass, Boolean> func = this::equals;
-		return func.apply(cl);
+		Predicate<MyClass> func = this::equals;
+		return func.test(cl);
 	}
 }
