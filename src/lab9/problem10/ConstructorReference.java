fd7d77931c35c6c7c293472f12817138df30aa5b
Binh Tran <binh12121989@gmail.com>
2018-09-23 14:00:31 -0500

    Fix Lab9.problem10: remove unsued code

--- src/lab9/problem10/ConstructorReference.java
+++ src/lab9/problem10/ConstructorReference.java
@@ -1,8 +1,5 @@
 package lab9.problem10;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
